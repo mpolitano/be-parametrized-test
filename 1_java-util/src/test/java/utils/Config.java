6d3c16e12c98dcd4e22de50586a5d525a2c6d293
Mariano Politano <marianopolitano@gmail.com>
2022-10-12 20:09:18 +0200

    add config properties

---
+++ 1_java-util/src/test/java/utils/Config.java
@@ -0,0 +1,29 @@
+package utils;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Config {
+
+    
+    
+	public static int scope;
+
+    public static void readEnvironmentVariables() {
+
+        String propFile = System.getProperty("properties", "config.properties");
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream(propFile));
+            scope = Integer.parseInt(props.getProperty("scope"));
+         
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+ 
+
+}
