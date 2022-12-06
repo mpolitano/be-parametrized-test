192bbf182d16cd52148054e339c788ab8a3d4f87
Mariano Politano <marianopolitano@gmail.com>
2022-12-06 20:46:23 +0100

    clean all repository

--- scripts/readMutation.py
+++ scripts/readMutation.py
@@ -6,31 +6,13 @@ from os.path import join,isfile
 import re   # regex module
 import os
 
-
-# def file():
-   # return "build/reports/pitest/mutations.csv"
-# def parser(seed, package, clazz, budget, tool, numberTests, suffix =".csv"):
 def parser( file, suffix =".csv"):
 	mutations=0
 	output = "result.txt"
 	fname = file
-    # row[1] = package
-    # row[2] = class
-    # row[3] = Instruction_miss
-    # row[4] = Instruction_coverage
-    # row[5] = branch_miss
-    # row[6] = branch_coverage
-    # row[7] = line_miss
-    # row[8] = line_coverage
 
 	df = pd.read_csv(fname, header=None)
-    #df = df.loc[df[1] == casestudy] 
-    # out = open("result.txt", "w")
 	out = open(output, "a+")
-	# out.write("-----------------")
-	# out.write(seed)
-	# out.write("-----------------")
-	# out.write("\n")
 	discard = ["readObject" "writeObject" "repOK"]
 	for index, row in df.iterrows():
 		if row[5] == 'KILLED':
@@ -45,48 +27,8 @@ def parser( file, suffix =".csv"):
 	# out.write('mutatans: {}\n'.format(mutations))
 	return mutations
 
-	# out.close()
-
-
-	# out.close()
-	# for f in listdir(directory):
-	# fname = join(directory, "report.csv")
-	# # if isfile(fname) and fname.endswith( suffix ):
-	# print('Processing: {}'.format(fname))
-	# df = pd.read_csv(fname)
-	# out = open("result.txt", "a")
-	# out.write('{}: {}\n'.format(fname, df.iloc[0,1]))
-	# 	# else:
-	# 	# 	print('Warning: file {} does not exist or no is .csv'.format(f))
-	# out.close()
-
-
-# def grepForTest(dir):
-# 	n = 0
-# 	print(dir)	
-# 	count = 0
-# 	for filename in os.listdir(dir):
-# 		file1 = open(filename, 'r')
-# 		Lines = file1.readlines()
-# 		# Strips the newline character
-# 		for line in Lines:
-# 			if re.match("@Test", line):
-# 				n+=1
-# 	return n
-            
 if __name__ == '__main__':
-	# if len(sys.argv) != 3:
- #   		print("Run the command with the DIRECTORY where the files are located and the NAME of the output file\n")
- #   		print("Example python script files/ result.txt\n")
-	# output = "result.txt"
 	file = sys.argv[1]
-	# package = sys.argv[2]
-	# clazz = sys.argv[3]
-	# seed = sys.argv[4]
-	# tool = sys.argv[5]
-	# numberTests = sys.argv[6]
-
-
 	print(parser(file))
 
 
--- scripts/run-coverage-randoop.sh
+++ scripts/run-coverage-randoop.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-
 project=$1
 class=$2
 budget=$3
 tool=$4
-
-# methodnum=$5
-# method=$6
 
 projectsdir=$BE_EXP_SRC
 scriptsdir=$projectsdir/scripts
@@ -21,147 +17,90 @@ if [[ -d $resultsdir ]]; then
 fi
 mkdir -p $resultsdir
 explog=$resultsdir/log.txt
-# echo "$method" > $resultsdir/method.txt
 
 pushd $projectdir > /dev/null
 
 echo ""
 echo "> Starting experiment: $@"
 
-clean_and_compile
 packagename=${class%\.*}
-
-
-
+dirPackage=${packagename//.//}
 testSource=$projectdir/src/test/java/
 rm -r $testSource
 mkdir $testSource
+
+
 #Need generate serialize
 cmd="$scriptsdir/gen-$tool.sh $project $class $budget graph builders > $explog"
 echo ""
 echo "> Generating/serialize tests: $cmd"
 bash -c "$cmd" 
 
+
 if [[ $tool == "randoop" ]]; then 
 	# cmd="cp -r $projectdir/$tool-tests $resultsdir"
 	cmd="cp -r $projectdir/$tool-tests/* $testSource"
-fi
-
-if [[ $tool == "randoop-serialize-builders" ]]; then 
+else #need parameterized tests. Depends with builders or no builders
 	sed -i'' -e "s/scope=.*/scope=$budget/g" config.properties
 	sed -i'' -e "s/tool=.*/tool=$tool/g" config.properties
-	cmd="cp -r $projectdir/testParametrized/* $testSource"
-fi
-if [[ $tool == "randoop-serialize" ]]; then 
-	echo "Serialize"
-	sed -i'' -e "s/scope=.*/scope=$budget/g" config.properties
-	sed -i'' -e "s/tool=.*/tool=$tool/g" config.properties
-	cmd="cp -r $projectdir/testParametrized/* $testSource"
-fi
-	echo ""
-	echo "> Saving tests: $cmd"
-	bash -c "$cmd" 
-
-	# cmd="mvn clean 	test >> $explog"
-	# echo ""
-	# echo "> Running tests, Coverage and Mutation: $cmd"
-	# bash -c "$cmd"
-
-	cmd="mvn clean test-compile test  >> $explog"
-	echo ""
-	echo "> Running test $cmd"
-	bash -c "$cmd"
+	sed -i'' -e "s/clazz=.*/clazz=$clazz/g" config.properties
 	
-	cmd="cp -r $resultsdir/tests.txt $resultsdir/testsCount.txt"
+	mkdir -p $testSource/${dirPackage}/
+	cmd="cp -r $projectdir/testParametrized/${dirPackage}/* $testSource/${dirPackage}/"
 	echo ""
-	echo "> Save Test count $cmd"
-	bash -c "$cmd"
-
-	cmd="cp -r $projectdir/target/surefire-reports $resultsdir"
-	echo ""
-	echo "> Save Test runner $cmd"
-	bash -c "$cmd"
-
-
-	cmd="mvn test jacoco:report -Dpackage=${packagename} >> $explog"
-	echo ""
-	echo "> Running jacoco $cmd"
-	bash -c "$cmd"
-	
-
-	echo ""
-	echo "> Saving Jacoco: $cmd"
-	cmd="cp -r $projectdir/target/site/*/* $resultsdir"
+	echo "> Copy tests parameterized: $cmd"
 	bash -c "$cmd" 
 
-
-	cmd="timeout 3600 mvn clean test-compile org.pitest:pitest-maven:mutationCoverage -Dpackage=${packagename} >> $explog"
+	cmd="cp -r $projectdir/testParametrized/utils $testSource/"
 	echo ""
-	echo "> Running pit $cmd"
-	bash -c "$cmd"
-
-
-	cmd="cp -r $projectdir/target/pit-reports $resultsdir"
-	echo ""
-	echo "> Saving pit: $cmd"
+	echo "> Copy utils for tests: $cmd"
 	bash -c "$cmd" 
+fi
 
 
-
-# if [[ $tool == "be-all" ]]; then 
-# 	md="cp -r $projectdir/$tool-tests $resultsdir"
-# 	echo ""
-# 	echo "> Saving tests: $cmd"
-# 	bash -c "$cmd" 
-# fi
-
-
-# cmd="cp -r $projectdir/$tool-tests $resultsdir"
+# cmd="mvn clean 	test >> $explog"
 # echo ""
-# echo "> Saving tests: $cmd"
-	# bash -c "$cmd" 
-
-# cmd="mvn jacoco:report -Dpackage=$packagename >> $explog"
-# echo ""
-# echo "> Running tests: $cmd"
+# echo "> Running tests, Coverage and Mutation: $cmd"
 # bash -c "$cmd"
 
-# cmd="mvn org.pitest:pitest-maven:mutationCoverage -Dpackage=$packagename >> $explog"
-# echo ""
-# echo "> Running tests: $cmd"
-# bash -c "$cmd"
+cmd="mvn clean test-compile test  >> $explog"
+echo ""
+echo "> Running test $cmd"
+bash -c "$cmd"
 
-# cmd="ant $tool-coverage >> $explog"
-# echo ""
-# echo "> Measuring coverage: $cmd"
-# bash -c "$cmd"
+cmd="cp -r $resultsdir/tests.txt $resultsdir/testsCount.txt"
+echo ""
+echo "> Save Test count $cmd"
+bash -c "$cmd"
 
-# Save results
-# cp -r build/test-report $resultsdir
-# cp -r build/jacoco $resultsdir
+cmd="cp -r $projectdir/target/surefire-reports $resultsdir"
+echo ""
+echo "> Save Test runner $cmd"
+bash -c "$cmd"
 
-# For evosuite to work with pit we have to disable automated mocks. So we have to regenerate tests
-# if [[ $tool == "evosuite" ]]; then
-#     clean_and_compile
-#     param2="nomocks"
-#     cmd="$scriptsdir/gen-$tool.sh $project $class $budget $param2 >> $explog"
-#     echo ""
-#     echo "> Generating tests: $cmd"
-#     bash -c "$cmd"
 
-#     cmd="cp -r $projectdir/$tool-tests $resultsdir/$tool-tests-$param2"
-#     echo ""
-#     echo "> Saving tests: $cmd"
-#     bash -c "$cmd" 
-# fi
+cmd="mvn test jacoco:report -Dpackage=${packagename} >> $explog"
+echo ""
+echo "> Running jacoco $cmd"
+bash -c "$cmd"
 
-# cmd="ant pit-beapi -Dpackage=$packagename >> $explog"
-# echo ""
-# echo "> Measuring mutation: $cmd"
-# bash -c "$cmd"
 
-# Save results
-# cp -r build/pitReports $resultsdir
+echo ""
+echo "> Saving Jacoco: $cmd"
+cmd="cp -r $projectdir/target/site/*/* $resultsdir"
+bash -c "$cmd" 
+
+
+cmd="timeout 3600 mvn clean test-compile org.pitest:pitest-maven:mutationCoverage -Dpackage=${packagename} >> $explog"
+echo ""
+echo "> Running pit $cmd"
+bash -c "$cmd"
+
+
+cmd="cp -r $projectdir/target/pit-reports $resultsdir"
+echo ""
+echo "> Saving pit: $cmd"
+bash -c "$cmd" 
 
 rm -r $projectdir/target/*
 popd > /dev/null
