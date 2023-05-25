730f317172871aa2e82b8336e159d59dd7a9b82f
Mariano Politano <marianopolitano@gmail.com>
2023-05-25 14:50:47 +0200

    new runner

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -24,46 +24,39 @@ scopes="4"
 
 projects="1_java-util"
 cases="java2.util2.treemap.TreeMap java2.util2.treemap.TreeSet java2.util2.hashmap.HashMap java2.util2.hashmap.HashSet java2.util2.linkedlist.LinkedList"
-# cases="java2.util2.treemap.TreeMap"
 techniques="randoop"
 scopes="5 10 20 40 150 300"
 run_many
 
-projects="1_java-util"
-cases="java2.util2.treemap.TreeMap java2.util2.treemap.TreeSet java2.util2.hashmap.HashMap java2.util2.hashmap.HashSet java2.util2.linkedlist.LinkedList"
-# cases="java2.util2.treemap.TreeMap"
 techniques="randoop-builders"
-scopes="5 10 20 40"
+scopes="5 10 20 40 150"
 run_many
 
-projects="1_java-util"
-cases="java2.util2.treemap.TreeMap java2.util2.treemap.TreeSet java2.util2.hashmap.HashMap java2.util2.hashmap.HashSet java2.util2.linkedlist.LinkedList"
-# cases="java2.util2.treemap.TreeMap"
 techniques="randoop-serialize"
-scopes="5 10 20 40"
-run_many
+scopes="5 10 20 40 150"
+# run_many
 
-projects="1_java-util"
-cases="java2.util2.treemap.TreeMap java2.util2.treemap.TreeSet java2.util2.hashmap.HashMap java2.util2.hashmap.HashSet java2.util2.linkedlist.LinkedList"
-# cases="java2.util2.treemap.TreeMap"
 techniques="beapi"
-scopes="3 4 5"
-run_many
+scopes="3 4 5 6 7 8"
+# run_many
 
 
 projects="2_apache"
 cases="org.apache.commons.collections4.list.NodeCachingLinkedList"
-techniques="randoop-serialize randoop-builders randoop"
-# techniques="beapi"
-scopes="10"
-# scopes="4"
+techniques="randoop"
+scopes="5 10 20 40 150 300"
+run_many
+
+techniques="randoop-builders"
+scopes="5 10 20 40 150 300"
+run_many
+
+techniques="randoop-serialize"
+scopes="5 10 20 40 150"
 # run_many
 
-projects="2_apache"
-cases="org.apache.commons.collections4.list.NodeCachingLinkedList"
-techniques="randoop-serialize randoop-builders randoop"
 techniques="beapi"
-# scopes="10"
-scopes="3"
+scopes="3 4 5 6 7 8"
 # run_many
+
 
